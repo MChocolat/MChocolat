25fe79ba4dfa69479fa8e9f08caa19973c203c82
Geo <geoving.gerard@gmail.com>
2014-09-16 16:00:56 -0400

    repush

--- helloworld.php
+++ helloworld.php
@@ -41,16 +41,16 @@
 </dl>
 <div class="tabs-content">
   <div class="content active" id="panel1">
-    <p>This is the first panel of the basic tab example. This is the first panel of the basic tab example.</p>
+    <p>This is the first panel of a basic tab example. This is the first panel of the basic tab example.</p>
   </div>
   <div class="content" id="panel2">
-    <p>This is the second panel of the basic tab example. This is the second panel of the basic tab example.</p>
+    <p>This is the second panel of a basic tab example. This is the second panel of the basic tab example.</p>
   </div>
   <div class="content" id="panel3">
-    <p>This is the third panel of the basic tab example. This is the third panel of the basic tab example.</p>
+    <p>This is the third panel of a basic tab example. This is the third panel of the basic tab example.</p>
   </div>
   <div class="content" id="panel4">
-    <p>This is the fourth panel of the basic tab example. This is the fourth panel of the basic tab example.</p>
+    <p>This is the fourth panel of a basic tab example. This is the fourth panel of the basic tab example.</p>
   </div>
 </div>
 		  	<div class="small-8 large-centered columns">
