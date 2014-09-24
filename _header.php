aae750d648f9061ebf53bfce7d2243b51fd0ca71
rvz7 <rvbroussard@gmail.com>
2014-09-23 21:17:21 -0400

    Adjusting SQL connection string for server use

--- _header.php
+++ _header.php
@@ -1,5 +1,5 @@
 <?php
-$con=mysqli_connect("173.194.107.202","root","mchocolat","test");
+$con=mysqli_connect("","root","mchocolat","test");
 // Check connection
 if (mysqli_connect_errno()) {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
