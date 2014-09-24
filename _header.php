169e583b966d1ab2d2f00db9920c214759ec491d
rvz7 <rvbroussard@gmail.com>
2014-09-23 21:25:11 -0400

    updated DB connection string for GAE

--- _header.php
+++ _header.php
@@ -1,5 +1,6 @@
 <?php
-$con=mysqli_connect("localhost","root","mchocolat","test");
+$con=mysql_connect(':cloudsql/inventorymchocolat:mchocolat',"root","");
+mysql_select_db('test');
 // Check connection
 if (mysqli_connect_errno()) {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
