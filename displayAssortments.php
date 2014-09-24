75510bb096e577d41ad4063d635b14a96d5ece68
rvz7 <rvbroussard@gmail.com>
2014-09-24 12:19:52 -0400

    trying to fix tabs

--- displayAssortments.php
+++ displayAssortments.php
@@ -11,12 +11,12 @@ include "_header.php";
       <script src="js/vendor/modernizr.js"></script>
 </head>
 <body>
-<ul class="tabs" data-tab role="tablist">
-  <li class="tab-title active" role="presentational" ><a href="#panel2-1" role="tab" tabindex="0" aria-selected="true" controls="panel2-1">Tab 1</a></li>
-  <li class="tab-title" role="presentational" ><a href="#panel2-2" role="tab" tabindex="0"aria-selected="false" controls="panel2-2">Tab 2</a></li>
-  <li class="tab-title" role="presentational"><a href="#panel2-3" role="tab" tabindex="0" aria-selected="false" controls="panel2-3">Tab 3</a></li>
-  <li class="tab-title" role="presentational" ><a href="#panel2-4" role="tab" tabindex="0" aria-selected="false" controls="panel2-4">Tab 4</a></li>
-</ul>
+<dl class="tabs" data-tab role="tablist">
+  <dd class="tab-title active" role="presentational" ><a href="#panel2-1" role="tab" tabindex="0" aria-selected="true" controls="panel2-1">Tab 1</a></dd>
+  <dd class="tab-title" role="presentational" ><a href="#panel2-2" role="tab" tabindex="0"aria-selected="false" controls="panel2-2">Tab 2</a></dd>
+  <dd class="tab-title" role="presentational"><a href="#panel2-3" role="tab" tabindex="0" aria-selected="false" controls="panel2-3">Tab 3</a></dd>
+  <dd class="tab-title" role="presentational" ><a href="#panel2-4" role="tab" tabindex="0" aria-selected="false" controls="panel2-4">Tab 4</a></dd>
+</dl>
 <div class="tabs-content">
   <section role="tabpanel" aria-hidden="false" class="content active" id="panel2-1">
     <h2>First panel content goes here...</h2>
@@ -55,7 +55,7 @@ contents: <input type="text" name="contents">
 </form>
 
  </section>
-  <section role="tabpanel" aria-hidden="false" class="content" id="panel2-2">
+  <section role="tabpanel" aria-hidden="true" class="content" id="panel2-2">
     <h2>Second panel content goes here...</h2>
   </section>
   <section role="tabpanel" aria-hidden="true" class="content" id="panel2-3">
