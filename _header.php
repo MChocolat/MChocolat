0c8a709280bfe49e9314148cd4510c7012b6ffa6
rvz7 <rvbroussard@gmail.com>
2014-09-23 21:39:44 -0400

    Revert d17fa39..8ccda5f
    This rolls back to commit d17fa39294c06b81e3dce1c1b9c25a9b2ccd33fa.

---
+++ _header.php
@@ -0,0 +1,11 @@
+<?php
+$con=new mysqli(null, 'root', '', 'test', null, '/cloudsql/inventorymchocolat:mchocolat');
+
+// Check connection
+if (mysqli_connect_errno()) {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+
+date_default_timezone_set('EST');
+ini_set('display_errors', 'On');
+?>
