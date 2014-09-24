d6699b023eda5a8811f7952f52fb4c542eedebbc
Christina <christina.snyder11@gmail.com>
2014-09-24 15:10:42 -0400

    still testing

--- displayAssortments.php
+++ displayAssortments.php
@@ -32,7 +32,7 @@ echo "function worked";
 
 
 <form action="/insertAssortments" method="post" width="25">
-quantity: <input type="int" name="quantity"><br>
+quantitytestttttt: <input type="int" name="quantity"><br>
 contents: <input type="text" name="contents">
 <input type="submit">
 </form>
