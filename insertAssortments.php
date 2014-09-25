354c75f8f8a10c1491249a09129bc47b3fbeb6a5
rvz7 <rvbroussard@gmail.com>
2014-09-25 10:04:45 -0400

    Shortened insert code using function and testing

--- insertAssortments.php
+++ insertAssortments.php
@@ -1,5 +1,5 @@
 <?php
-include "_header.php";
+include "functions.php";
 ?>
 <!DOCTYPE html>
 <html>
@@ -7,38 +7,26 @@ include "_header.php";
 	<title>Assortment Inserted</title>
 </head>
 <body>
-<?php
+	<?php
+		echo "connected... ";
 
+		// escape variables for security
+		$quantity = mysqli_real_escape_string($con, $_POST['quantity']);
+		$contents = mysqli_real_escape_string($con, $_POST['contents']);
+		$date = date("Y-m-d");
 
-global $con;
+		add2Assortments($date, $quantity, $contents);
 
-echo "connected... ";
+		echo "1 record added <br />";
+		echo "Added: <br />";
+		echo "Date: $date <br />";
+		echo "quantity: $quantity <br />";
+		echo "contents: $contents <br />";
+	?>
 
-
-// escape variables for security
-$quantity = mysqli_real_escape_string($con, $_POST['quantity']);
-$contents = mysqli_real_escape_string($con, $_POST['contents']);
-$date = date("Y-m-d");
-
-$sql="INSERT INTO assortments (AID, DOC, quantity, contents)
-VALUES (null, '$date', '$quantity', '$contents');";
-
-if (!mysqli_query($con,$sql)) {
-  die('Error: ' . mysqli_error($con));
-}
-echo "1 record added <br />";
-echo "Added: <br />";
-echo "Date: $date <br />";
-echo "quantity: $quantity <br />";
-echo "contents: $contents <br />";
-
-mysqli_close($con);
-
-?>
-
-<form action="/displayAssortments" method="get">
-<input type="submit" value="Return to displayAssortments">
-</form>
+	<form action="/displayAssortments" method="get">
+	<input type="submit" value="Return to displayAssortments">
+	</form>
 
 </body>
 </html>
