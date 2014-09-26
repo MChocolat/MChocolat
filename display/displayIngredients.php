aed5dc696a59f50448f0f66fc85b92096fcd9de1
rvz7 <rvbroussard@gmail.com>
2014-09-26 00:06:46 -0400

    Revert 3082816..1ba31d5
    This rolls back to commit 308281695478447a820115eafc8fcc843fe2a038.

--- display/displayIngredients.php
+++ display/displayIngredients.php
@@ -7,16 +7,6 @@ include "_header.php";
 			<h1>Ingredients</h1>
 
 			<?php table_displayAllIngredients(); ?>
-
-
-			<form action="/insert/insertAssortments" method="post" width="25">
-                UPC: <input type="int" name="quantity"><br>
-                Expiration Date: <input type="text" name="contents">
-                Price: <input type="number" name="quantity"><br>
-                Distributor: <input type="text" name="quantity"><br>
-                SubIngr: <input type="textarea" name="quantity"><br>
-                <input type="submit">
-                </form>
 		</div>
 	</body>
 </html>
