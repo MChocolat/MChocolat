a8501e29f4678b796d02cf4dcb1cf03e2d942ea9
rvz7 <rvbroussard@gmail.com>
2014-09-25 22:38:30 -0400

    fixed table spacing on page

--- display/displayAssortments.php
+++ display/displayAssortments.php
@@ -4,19 +4,21 @@ include "_header.php";
 <!DOCTYPE html>
 <html>
 <body>
-        <h2>Assortments </h2>
-      
-        <?php
-        table_displayAllAssortments();
-        echo "function worked";
-        ?>
+        <div class="row">
+                <h2>Assortments </h2>
+              
+                <?php
+                table_displayAllAssortments();
+                echo "function worked";
+                ?>
 
 
-        <form action="/insertAssortments" method="post" width="25">
-        quantity: <input type="int" name="quantity"><br>
-        contents: <input type="text" name="contents">
-        <input type="submit">
-        </form>
+                <form action="/insertAssortments" method="post" width="25">
+                quantity: <input type="int" name="quantity"><br>
+                contents: <input type="text" name="contents">
+                <input type="submit">
+                </form>
+        </div>
 
 
 </body>
