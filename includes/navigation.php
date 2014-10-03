be427be2d43b0440ba1daf666ba7325360d803c4
Aaryn Glosterm <agg1991@yahoo.com>
2014-10-03 13:55:59 -0400

    Renamed Files
    Renamed

--- includes/navigation.php
+++ includes/navigation.php
@@ -3,11 +3,11 @@
 		<p></p>
 		<div class="button-bar">
 		<ul class="button-group">
-	    	<li><a href="/display/rawIngredients.php" class="small button">Raw Ingredients</a></li>
+	    	<li><a href="/display/ingredients.php" class="small button">Raw Ingredients</a></li>
 	    	<li><a href="/display/recipes.php" class="small button">Recipes</a></li>
 	    	<li><a href="/display/batches.php" class="small button">Batches</a></li>
 	    	<li><a href="/display/packaging.php" class="small button">Packaging</a></li>
-	    	<li><a href="/display/finalProducts.php" class="small button">Final Products</a></li>
+	    	<li><a href="/display/boxLabels.php" class="small button">Final Products</a></li>
 		  	</ul>
 		</div>
   	</div>
--- index.php
+++ index.php
@@ -1,38 +1,7 @@
 <html class="no-js" lang="en">
-	<head>
-    	<meta charset="utf-8" />
-    	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    	<title>M Chocolat Inventory Management System</title>
-    	<link rel="stylesheet" href="css/foundation.css" />
-    	<link rel="stylesheet" href="stylesheets/main.css"/>
-    	<link rel="stylesheet" href="test.css"/>
-    	<script src="js/vendor/modernizr.js"></script>
-  	</head>
+	<?php include("/includes/header.php"); ?>
   
   	<body>
-		<nav class="top-bar" data-topbar>
-			<ul class="title-area">
-				<li class="name">
-					<img src="img/MchocIconLogo.png" height="30" alt="">
-	  				<!-- <img src="http://lorempixel.com/45/45/" alt=""> -->
-				</li>
-			</ul>
-			<section class="top-bar-section">
-				<!-- Right Nav Section -->
-				<ul class="right">
-		  			<li><a href="#Logout">Logout</a></li>
-		  			<li><a href="#Settings">Settings</a></li>
-				</ul>
-
-				<!-- Left Nav Section -->
-				<ul class="left">
-		  			<div class="large-1 columns">  
-		    			<h4><a href="#Administrator">Administrator</a></h4>
-		  			</div>
-				</ul>
-			</section>
-		</nav>
-
 	<div class = "twelve columns centered">
 		<table>
 			<tr>
@@ -40,11 +9,11 @@
 				<th colspan = "2" class = "text-center" ><h2>Batches</h2></th>
 			</tr>
 			<tr>
-				<td style="text-align: center;"><a href = "page" class = "large button">Add new</a></td>
-				<td style="text-align: center;"><a href = "page" class = "large button">View/Search all</a></td>
+				<td style="text-align: center;"><a href = "/display/addIngredient.php" class = "large button">Add new</a></td>
+				<td style="text-align: center;"><a href = "/display/ingredients.php" class = "large button">View/Search all</a></td>
 				
-				<td style="text-align: center;"><a href = "page" class="large button">Make new</a></td>
-				<td style="text-align: center;"><a href = "page" class="large button">View/Search all</a></td>
+				<td style="text-align: center;"><a href = "/display/addBatches.php" class="large button">Make new</a></td>
+				<td style="text-align: center;"><a href = "/display/batches.php" class="large button">View/Search all</a></td>
 			</tr>
 
 			<tr>
@@ -52,11 +21,11 @@
 				<th colspan = "2" class = "text-center" ><h2>Box Labels</h2></th>
 			</tr>
 			<tr>
-				<td style="text-align: center;"><a href = "page" class = "large button">Make new</a></td>
-				<td style="text-align: center;"><a href = "page" class = "large button">View/Search all</a></td>
+				<td style="text-align: center;"><a href = "/display/addRecipe.php" class = "large button">Make new</a></td>
+				<td style="text-align: center;"><a href = "/display/recipes.php" class = "large button">View/Search all</a></td>
 				
-				<td style="text-align: center;"><a href = "page" class = "large button">Make new</a></td>
-				<td style="text-align: center;"><a href = "page" class = "large button">View/Search all</a></td>
+				<td style="text-align: center;"><a href = "/display/createBoxLabel.php" class = "large button">Make new</a></td>
+				<td style="text-align: center;"><a href = "/display/boxLabels.php" class = "large button">View/Search all</a></td>
 			</tr>
 		</table>
 	</div>
