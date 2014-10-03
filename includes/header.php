7f75b517925db3ac26bd39d4b618c06777d837bc
Geo <geoving.gerard@gmail.com>
2014-10-03 13:35:13 -0400

    Made Changes to Header and Navbar php

--- includes/header.php
+++ includes/header.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:display/header.php
 <nav class="top-bar foundation-bar" data-topbar>
   <ul class="title-area">
     <li class="name">
@@ -16,30 +15,4 @@
       	<li><a  href="login.html">Logout</a></li>
     </ul>
   </section>
-=======
-<!-- Navigation -->
-
-<nav class="top-bar" data-topbar>
-			<ul class="title-area">
-				<li class="name">
-					<a href="/index.php">
-						<img src="/img/MchocIconLogo.png" alt="Home">
-					</a>
-				</li>
-			</ul>
-			<section class="top-bar-section">
-				<!-- Right Nav Section -->
-				<ul class="right">
-		  			<li><a href="#Logout">Logout</a></li>
-		  			<li><a href="#Settings">Settings</a></li>
-				</ul>
-
-				<!-- Left Nav Section -->
-				<ul class="left">
-		  			<div class="large-1 columns">  
-		    			<h4><a href="#Administrator">Administrator</a></h4>
-		  			</div>
-				</ul>
-			</section>
->>>>>>> FETCH_HEAD:includes/header.php
 </nav>
