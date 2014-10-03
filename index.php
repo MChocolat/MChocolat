54ad677a081ad84a724addad6f4f6111039b4597
Geo <geoving.gerard@gmail.com>
2014-10-03 14:52:35 -0400

    test.

--- index.php
+++ index.php
@@ -7,8 +7,6 @@ echo "included stuff"; ?>
     	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
     	<title>M Chocolat Inventory Management System</title>
     	<link rel="stylesheet" href="css/foundation.css" />
-    	<link rel="stylesheet" href="stylesheets/main.css"/>
-    	<link rel="stylesheet" href="test.css"/>
     	<script src="js/vendor/modernizr.js"></script>
   	</head>
   	
