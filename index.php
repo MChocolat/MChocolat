fa2191e8d973bae439d4565cd942315ce24ebc85
Geo <geoving.gerard@gmail.com>
2014-10-06 12:34:49 -0400

    homepage formatting test

--- index.php
+++ index.php
@@ -1,20 +1,29 @@
 <?php include("header.php");
 ?>
 <html class="no-js" lang="en">
+
+	<head>
+    	<meta charset="utf-8" />
+    	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    	<title>Information Management System</title>
+    	<link rel="stylesheet" href="css/foundation.css" />
+    	<script src="js/vendor/modernizr.js"></script>
+  	</head>
   	
 	<body>
-		<div class = "small-12 columns centered">
+
+		<div class="small-12 columns">
 			<table>
 				<tr>
 					<th colspan = "2" class = "text-center" ><h2>Ingredients</h2></th>
 					<th colspan = "2" class = "text-center" ><h2>Batches</h2></th>
 				</tr>
 				<tr>
-					<td style="text-align: center;"><a href = "/display/addIngredient.php" class = "large button">Add new</a></td>
-					<td style="text-align: center;"><a href = "/display/ingredients.php" class = "large button">View/Search all</a></td>
+					<td style="text-align: center;"><a href = "/display/addIngredient.php" class = "medium button">Add new</a></td>
+					<td style="text-align: center;"><a href = "/display/ingredients.php" class = "medium button">View/Search all</a></td>
 					
-					<td style="text-align: center;"><a href = "/display/addBatches.php" class="large button">Make new</a></td>
-					<td style="text-align: center;"><a href = "/display/batches.php" class="large button">View/Search all</a></td>
+					<td style="text-align: center;"><a href = "/display/addBatches.php" class= "medium button">Make new</a></td>
+					<td style="text-align: center;"><a href = "/display/batches.php" class= "medium button">View/Search all</a></td>
 				</tr>
 
 				<tr>
@@ -22,11 +31,11 @@
 					<th colspan = "2" class = "text-center" ><h2>Box Labels</h2></th>
 				</tr>
 				<tr>
-					<td style="text-align: center;"><a href = "/display/addRecipe.php" class = "large button">Make new</a></td>
-					<td style="text-align: center;"><a href = "/display/recipes.php" class = "large button">View/Search all</a></td>
+					<td style="text-align: center;"><a href = "/display/addRecipe.php" class = "medium button">Make new</a></td>
+					<td style="text-align: center;"><a href = "/display/recipes.php" class = "medium button">View/Search all</a></td>
 					
-					<td style="text-align: center;"><a href = "/display/createBoxLabel.php" class = "large button">Make new</a></td>
-					<td style="text-align: center;"><a href = "/display/boxLabels.php" class = "large button">View/Search all</a></td>
+					<td style="text-align: center;"><a href = "/display/createBoxLabel.php" class = "medium button">Make new</a></td>
+					<td style="text-align: center;"><a href = "/display/boxLabels.php" class = "medium button">View/Search all</a></td>
 				</tr>
 			</table>
 		</div>
