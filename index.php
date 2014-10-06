b3fddca49ce84c276d5d2a9aecf92aad9af058de
Geo <geoving.gerard@gmail.com>
2014-10-06 13:43:30 -0400

    homepage formatting test#8

--- index.php
+++ index.php
@@ -1,46 +1,67 @@
 <?php include("header.php");
 ?>
 <html class="no-js" lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Information Management System</title>
+        <link rel="stylesheet" href="css/foundation.css" />
+        <script src="js/vendor/modernizr.js"></script>
+    </head>
 
-	<head>
-    	<meta charset="utf-8" />
-    	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    	<title>Information Management System</title>
-    	<link rel="stylesheet" href="css/foundation.css" />
-    	<script src="js/vendor/modernizr.js"></script>
-  	</head>
-  	
-	<body>
-		
-		<div class="large-12 columns">
-			<table>
-				<tr>
-					<th colspan = "2" class = "text-center" ><h3>Ingredients</h3></th>
-					<th colspan = "2" class = "text-center" ><h3>Batches</h3></th>
-				</tr>
-				<tr>
-					<td style="text-align: center;"><a href = "/display/addIngredient.php" class = "medium button">Add new</a></td>
-					<td style="text-align: center;"><a href = "/display/ingredients.php" class = "medium button">View/Search all</a></td>
-					
-					<td style="text-align: center;"><a href = "/display/addBatches.php" class= "medium button">Make new</a></td>
-					<td style="text-align: center;"><a href = "/display/batches.php" class= "medium button">View/Search all</a></td>
-				</tr>
-
-				<tr>
-					<th colspan = "2" class = "text-center" ><h3>Recipes</h3></th>
-					<th colspan = "2" class = "text-center" ><h3>Box Labels</h3></th>
-				</tr>
-				<tr>
-					<td style="text-align: center;"><a href = "/display/addRecipe.php" class = "medium button">Make new</a></td>
-					<td style="text-align: center;"><a href = "/display/recipes.php" class = "medium button">View/Search all</a></td>
-					
-					<td style="text-align: center;"><a href = "/display/createBoxLabel.php" class = "medium button">Make new</a></td>
-					<td style="text-align: center;"><a href = "/display/boxLabels.php" class = "medium button">View/Search all</a></td>
-				</tr>
-			</table>
-		</div>
-		<script src="js/vendor/jquery.js"></script>
-	    <script src="js/foundation.min.js"></script>
-	    <script> $(document).foundation(); </script>
- 	 </body>
+    <body style="position: relative; min-height: 100%; top: 0px;">
+        <section id="portfolio" data-scroll-reveal="" data-scroll-reveal-initialized="true" data-scroll-reveal-complete="true">
+            <div class="row">
+                <div class="small-12 columns">
+                    <p id="loading" style="opacity: 0;"></p>
+                    <ul class="large-block-grid-2" id="option">
+                        <li class="mchocolat-option">
+                            <a href = "/display/addIngredient.php" class = "medium button">Add new</a>
+                                <!--<img src="https://d13yacurqjgara.cloudfront.net/users/49180/screenshots/1746569/dribbble-6sense.png" alt="Shot on Dribbble.com"> -->
+                            </a>
+                        </li>
+                        <li class="mchocolat-option">
+                            <a href = "/display/ingredients.php" class = "medium button">View/Search all</a>
+                                <!--<img src="https://d13yacurqjgara.cloudfront.net/users/49180/screenshots/1686202/search_options.png" alt="Shot on Dribbble.com"> -->
+                            </a>
+                        </li>
+                        <li class="mchocolat-option">
+                            <a href = "/display/addBatches.php" class= "medium button">Make new</a>
+                                <!--<img src="https://d13yacurqjgara.cloudfront.net/users/49180/screenshots/1606123/alliance-dribbble.png" alt="Shot on Dribbble.com"> -->
+                            </a>
+                        </li>
+                        <li class="mchocolat-option">
+                            <a href = "/display/batches.php" class= "medium button">View/Search all</a>
+                                <!--<img src="https://d13yacurqjgara.cloudfront.net/users/49180/screenshots/1591204/lens-dribbble.png" alt="Shot on Dribbble.com"> -->
+                            </a>
+                        </li>
+                        <li class="mchocolat-option">
+                            <a href = "/display/addRecipe.php" class = "medium button">Make new</a>
+                                <!--<img src="https://d13yacurqjgara.cloudfront.net/users/49180/screenshots/1582396/calculator-dribbble.png" alt="Shot on Dribbble.com"> -->
+                            </a>
+                        </li>
+                        <li class="mchocolat-option">
+                            <a href = "/display/recipes.php" class = "medium button">View/Search all</a>
+                                <!--<img src="https://d13yacurqjgara.cloudfront.net/users/49180/screenshots/1532663/budget-app.png" alt="Shot on Dribbble.com"> -->
+                            </a>
+                        </li>
+                        <li class="mchocolat-option">
+                            <a href = "/display/createBoxLabel.php" class = "medium button">Make new</a>
+                                <!--<img src="https://d13yacurqjgara.cloudfront.net/users/49180/screenshots/1532279/sra-logo.png" alt="Shot on Dribbble.com"> -->
+                            </a>
+                        </li>
+                        <li class="mchocolat-option">
+                            <a href = "/display/boxLabels.php" class = "medium button">View/Search all</a>
+                                <!--<img src="https://d13yacurqjgara.cloudfront.net/users/49180/screenshots/1505999/arise.png" alt="Shot on Dribbble.com"> -->
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </section>
+        
+        <script src="js/vendor/jquery.js"></script>
+        <script src="js/foundation.min.js"></script>
+        <script> $(document).foundation(); </script>
+    </body>
 </html>
