7d3708b4ce145cc9475d93f74af032caef8626c6
Geo <geoving.gerard@gmail.com>
2014-10-15 14:41:27 -0400

    navbar changes

--- navigation.php
+++ navigation.php
@@ -8,8 +8,8 @@
 		    	<li><a href="/display/ingredients.php" class="small button">Ingredients</a></li>
 		    	<li><a href="/display/recipes.php" class="small button">Recipes</a></li>
 		    	<li><a href="/display/batches.php" class="small button">Batches</a></li>
-		    	<!-- <li><a href="/display/packaging.php" class="small button">Packaging</a></li> -->
 		    	<li><a href="/display/boxLabels.php" class="small button">Box Labels</a></li>
+		    	<!-- <li><a href="/display/packaging.php" class="small button">Packaging</a></li> -->
 		  	</ul>
 		</div>
   	</div>
