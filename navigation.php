42994afad174f0cb0470bccfc2db797d7f596730
Geo <geoving.gerard@gmail.com>
2014-10-15 13:56:43 -0400

    navbar changes

--- navigation.php
+++ navigation.php
@@ -1,12 +1,16 @@
 <!-- Navigation -->
 <div class="row">
-		<p></p>
-		<ul class="button-group">
-			<li><a href="/" class="small button">Home</a></li>
-	    	<li><a href="/display/ingredients.php" class="small button">Ingredients</a></li>
-	    	<li><a href="/display/recipes.php" class="small button">Recipes</a></li>
-	    	<li><a href="/display/batches.php" class="small button">Batches</a></li>
-	    	<!-- <li><a href="/display/packaging.php" class="small button">Packaging</a></li> -->
-	    	<li><a href="/display/boxLabels.php" class="small button">Box Labels</a></li>
-	  	</ul>
+	<div id='toolbar'>
+		<div class='wrapper text-center'>
+			<p></p>
+			<ul class="button-group">
+				<li><a href="/" class="small button">Home</a></li>
+		    	<li><a href="/display/ingredients.php" class="small button">Ingredients</a></li>
+		    	<li><a href="/display/recipes.php" class="small button">Recipes</a></li>
+		    	<li><a href="/display/batches.php" class="small button">Batches</a></li>
+		    	<!-- <li><a href="/display/packaging.php" class="small button">Packaging</a></li> -->
+		    	<li><a href="/display/boxLabels.php" class="small button">Box Labels</a></li>
+		  	</ul>
+	  	</div>
+	</div>
 </div>
