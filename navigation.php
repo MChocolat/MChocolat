ffc117f9a8fd3a5fc0c76da381eb16a5c6ae706d
Geo <geoving.gerard@gmail.com>
2014-10-15 13:45:32 -0400

    Navbar Changes

--- navigation.php
+++ navigation.php
@@ -1,14 +1,26 @@
 <!-- Navigation -->
-<div class="row">
-<div class="large-12 column">
-		<p></p>
-		<ul class="button-group">
-			<li><a href="/" class="small button">Home</a></li>
-	    	<li><a href="/display/ingredients.php" class="small button">Ingredients</a></li>
-	    	<li><a href="/display/recipes.php" class="small button">Recipes</a></li>
-	    	<li><a href="/display/batches.php" class="small button">Batches</a></li>
-	    	<!-- <li><a href="/display/packaging.php" class="small button">Packaging</a></li> -->
-	    	<li><a href="/display/boxLabels.php" class="small button">Box Labels</a></li>
-		  	</ul>
-</div>
-</div>
+<!doctype html>
+<html class="no-js" lang="en">
+	<head>
+    	<meta charset="utf-8" />
+    	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    	<!--<title>Information Management System</title>-->
+    	<link rel="stylesheet" href="css/foundation.css" />
+    	<script src="js/vendor/modernizr.js"></script>
+  	</head>
+
+	<body>
+		<div class="row">
+			<div class="large-12 column">
+				<ul class="button-group">
+					<li><a href="/" class="small button">Home</a></li>
+			    	<li><a href="/display/ingredients.php" class="small button">Ingredients</a></li>
+			    	<li><a href="/display/recipes.php" class="small button">Recipes</a></li>
+			    	<li><a href="/display/batches.php" class="small button">Batches</a></li>
+			    	<!-- <li><a href="/display/packaging.php" class="small button">Packaging</a></li> -->
+			    	<li><a href="/display/boxLabels.php" class="small button">Box Labels</a></li>
+			  	</ul>
+			</div>
+		</div>
+	</body>
+</html>
