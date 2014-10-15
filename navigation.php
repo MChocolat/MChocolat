e7a8079148f731c5a31425ef96d63ea64f75c4a5
Geo <geoving.gerard@gmail.com>
2014-10-15 14:38:50 -0400

    navbar changes

--- navigation.php
+++ navigation.php
@@ -2,13 +2,15 @@
 <div class="row">
   	<div class="small-8 large-centered columns">
 		<p></p>
-		<ul class="button-group">
-			<li><a href="/" class="small button">Home</a></li>
-	    	<li><a href="/display/ingredients.php" class="small button">Ingredients</a></li>
-	    	<li><a href="/display/recipes.php" class="small button">Recipes</a></li>
-	    	<li><a href="/display/batches.php" class="small button">Batches</a></li>
-	    	<!-- <li><a href="/display/packaging.php" class="small button">Packaging</a></li> -->
-	    	<li><a href="/display/boxLabels.php" class="small button">Box Labels</a></li>
-	  	</ul>
+		<div class="button-bar">
+			<ul class="button-group">
+				<li><a href="/" class="small button">Home</a></li>
+		    	<li><a href="/display/ingredients.php" class="small button">Ingredients</a></li>
+		    	<li><a href="/display/recipes.php" class="small button">Recipes</a></li>
+		    	<li><a href="/display/batches.php" class="small button">Batches</a></li>
+		    	<!-- <li><a href="/display/packaging.php" class="small button">Packaging</a></li> -->
+		    	<li><a href="/display/boxLabels.php" class="small button">Box Labels</a></li>
+		  	</ul>
+		</div>
   	</div>
 </div>
