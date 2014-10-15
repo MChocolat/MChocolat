800bee0a230cb54a26c75d37f4093636385fc326
Geo <geoving.gerard@gmail.com>
2014-10-15 14:35:27 -0400

    navbar changes

--- navigation.php
+++ navigation.php
@@ -1,16 +1,14 @@
 <!-- Navigation -->
 <div class="row">
-	<div id='toolbar'>
-		<div class='wrapper text-center'>
-			<p></p>
-			<ul class="button-group">
-				<li><a href="/" class="small button">Home</a></li>
-		    	<li><a href="/display/ingredients.php" class="small button">Ingredients</a></li>
-		    	<li><a href="/display/recipes.php" class="small button">Recipes</a></li>
-		    	<li><a href="/display/batches.php" class="small button">Batches</a></li>
-		    	<!-- <li><a href="/display/packaging.php" class="small button">Packaging</a></li> -->
-		    	<li><a href="/display/boxLabels.php" class="small button">Box Labels</a></li>
-		  	</ul>
-	  	</div>
-	</div>
+  	<div class="small-8 large-centered columns">
+		<p></p>
+		<ul class="button-group">
+			<li><a href="/" class="small button">Home</a></li>
+	    	<li><a href="/display/ingredients.php" class="small button">Ingredients</a></li>
+	    	<li><a href="/display/recipes.php" class="small button">Recipes</a></li>
+	    	<li><a href="/display/batches.php" class="small button">Batches</a></li>
+	    	<!-- <li><a href="/display/packaging.php" class="small button">Packaging</a></li> -->
+	    	<li><a href="/display/boxLabels.php" class="small button">Box Labels</a></li>
+	  	</ul>
+  	</div>
 </div>
