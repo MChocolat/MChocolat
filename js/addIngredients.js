41067a8eec47d5b199fb6ea7b2ee20e1029f0b13
rvz7 <rvbroussard@gmail.com>
2014-10-16 16:48:29 -0400

    Final Fix?

--- js/addIngredients.js
+++ js/addIngredients.js
@@ -1,7 +1,7 @@
 $(document).ready( function () {
 	$(document).keypress(function(e) {
 		if(e.which == 13) {
-			if (document.activeElement == $('#addUpcInput')) {
+			if (document.activeElement.id == 'addUpcInput') {
 				alert ('You Pressed Enter in UPC!');
 			}
 		}
