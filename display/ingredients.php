ade7695f7b4846814a32a8420da4d3f8bd3b71c6
Geo <geoving.gerard@gmail.com>
2014-10-17 16:56:49 -0400

    addRecipe UI tweaks

--- display/ingredients.php
+++ display/ingredients.php
@@ -61,7 +61,7 @@
 					<p></p>
 
 					<div class="row ">
-						<div class="small-8 large-centered columns">
+						<div class="large-12 large-centered columns">
 						  	<ul class="button-group">
 							  <li><a href="#clear" class="tiny button disabled ">Clear</a></li>
 							  <li><a href="#barcode" class="tiny button">Scan Barcode</a></li>
