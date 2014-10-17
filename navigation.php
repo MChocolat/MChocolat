403522ae326cdd10738e8cacc4b591e277ac850c
Geo <geoving.gerard@gmail.com>
2014-10-17 14:43:40 -0400

    newNavbar test

--- navigation.php
+++ navigation.php
@@ -1,4 +1,5 @@
 <!-- Navigation -->
+<!--
 <div class="row">
   	<div class="small-8 large-centered columns">
 		<p></p>
@@ -9,8 +10,101 @@
 		    	<li><a href="/display/recipes.php" class="small button">Recipes</a></li>
 		    	<li><a href="/display/batches.php" class="small button">Batches</a></li>
 		    	<li><a href="/display/boxLabels.php" class="small button">Box Labels</a></li>
-		    	<!-- <li><a href="/display/packaging.php" class="small button">Packaging</a></li> -->
 		  	</ul>
 		</div>
   	</div>
+</div>
+
+-->
+
+<div class="columns small-12">
+    <a id="home" href="/en_us/">swatch</a>
+	<span id="mainNavToggle" class="hide-for-medium-up">
+		<a href="#" class="navtoggle">menu</a>
+	</span>
+    <nav id="mainNav" role="navigation">
+    	<ul>
+    		<li class="mainItem ">
+    			<span class="mainLink">
+    				<a>Ingredients</a>
+    			</span>
+    			<div class="megaFlyOut" style="top: 60px;">
+    				<div class="row">
+    					<div class="small-12 medium-4 large-4 columns">
+							<ul class="">
+								<li class="">
+									<a href="/display/addIngredient.php">Add New Ingredient</a>
+								</li>
+								<li class="">
+									<a href="/display/ingredients.php">View/Search All Ingredients</a>
+								</li>
+							</ul>
+    					</div>
+    				</div>
+    			</div>
+    		</li>
+
+    		<li class="mainItem ">
+    			<span class="mainLink">
+    				<a>Batches</a>
+    			</span>
+    			<div class="megaFlyOut" style="top: 60px;">
+    				<div class="row">
+    					<div class="small-12 medium-4 large-4 columns">
+							<ul class="">
+								<li class="">
+									<a href="/display/addBatch.php">Add New Batch</a>
+								</li>
+								<li class="">
+									<a href="/display/batches.php">View/Search All Batches</a>
+								</li>
+							</ul>
+    					</div>
+    				</div>
+    			</div>
+    		</li>
+		
+    		<li class="mainItem ">
+    			<span class="mainLink">
+    				<a>Recipes</a>
+    			</span>
+    			<div class="megaFlyOut" style="top: 60px;">
+    				<div class="row">
+    					<div class="small-12 medium-4 large-4 columns">
+							<ul class="">
+								<li class="">
+									<a href="/display/addRecipe.php">Add New Recipe</a>
+								</li>
+								<li class="">
+									<a href="/display/recipes.php">View/Search All Recipes</a>
+								</li>
+							</ul>
+    					</div>
+    				</div>
+    			</div>
+    		</li>
+
+    		<li class="mainItem ">
+    			<span class="mainLink">
+    				<a>Box Labels</a>
+    			</span>
+    			<div class="megaFlyOut" style="top: 60px;">
+    				<div class="row">
+    					<div class="small-12 medium-4 large-4 columns">
+							<ul class="">
+								<li class="">
+									<a href="/display/createBoxLabel.php">Add New Box Label</a>
+								</li>
+								<li class="">
+									<a href="/display/boxLabels.php">View/Search All Box Labels</a>
+								</li>
+							</ul>
+    					</div>
+    				</div>
+    			</div>
+    		</li>
+
+		</ul>
+	</nav>
+	<!--<div id="nav-overlay" style="height: 3009px;">&nbsp;</div>-->
 </div>
