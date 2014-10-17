590b89040153c45f9adf45b9cbf5556971cc375e
Geo <geoving.gerard@gmail.com>
2014-10-17 18:01:00 -0400

    radical navbar change

--- navigation.php
+++ navigation.php
@@ -12,47 +12,52 @@
 		<div class="row">
 		  	<div class="small-11 small-centered columns">
 				<p></p>
-				<ul class="button-group">
+				<ul class="button-group even-5">
 					<li>
 						<a href="#" class="button" data-dropdown="ingredients" data-options="is_hover:true">Ingredients</a>
-						<ul id="ingredients" class="f-dropdown" data-dropdown-content>
-						  <li><a href="/display/addIngredient.php">Add New Ingredient</a></li>
-						  <li><a href="/display/ingredients.php">View/Search All Ingredients</a></li>
-						</ul>
 					</li>
 
 			    	<li>
 						<a href="#" class="button" data-dropdown="batches" data-options="is_hover:true">Batches</a>
-						<ul id="batches" class="f-dropdown" data-dropdown-content>
-						  <li><a href="/display/addBatch.php">Add New Batch</a></li>
-						  <li><a href="/display/batches.php">View/Search All Batches</a></li>
-						</ul>
+
 			    	</li>
 
 			    	<li>
 			    		<a href="#" class="button" data-dropdown="recipes" data-options="is_hover:true">Recipes</a>
-						<ul id="recipes" class="f-dropdown" data-dropdown-content>
-						  <li><a href="/display/addRecipe.php">Add New Recipe</a></li>
-						  <li><a href="/display/recipes.php">View/Search All Recipes</a></li>
-						</ul>
+
 			    	</li>
 
 			    	<li>
 			    		<a href="#" class="button" data-dropdown="boxLabels" data-options="is_hover:true">Box Labels</a>
-						<ul id="boxLabels" class="f-dropdown" data-dropdown-content>
-						  <li><a href="/display/createBoxLabel.php">Add New Box Label</a></li>
-						  <li><a href="/display/boxLabels.php">View/Search All Box Labels</a></li>
-						</ul>
+
 			    	</li>
 
 			    	<li>
 			    		<a href="#" class="button" data-dropdown="packaging" data-options="is_hover:true">Packaging</a>
+
+			    	</li>
+				</ul>
+						<ul id="ingredients" class="f-dropdown" data-dropdown-content>
+						  <li><a href="/display/addIngredient.php">Add New Ingredient</a></li>
+						  <li><a href="/display/ingredients.php">View/Search All Ingredients</a></li>
+						</ul>
 						<ul id="packaging" class="f-dropdown" data-dropdown-content>
 						  <li><a href="/display/addPackaging.php">Add New Packaging Option</a></li>
 						  <li><a href="/display/packaging.php">View/Search Packaging Options</a></li>
 						</ul>
-			    	</li>
-			  	</ul>
+						<ul id="boxLabels" class="f-dropdown" data-dropdown-content>
+						  <li><a href="/display/createBoxLabel.php">Add New Box Label</a></li>
+						  <li><a href="/display/boxLabels.php">View/Search All Box Labels</a></li>
+						</ul>
+						<ul id="recipes" class="f-dropdown" data-dropdown-content>
+						  <li><a href="/display/addRecipe.php">Add New Recipe</a></li>
+						  <li><a href="/display/recipes.php">View/Search All Recipes</a></li>
+						</ul>
+						<ul id="batches" class="f-dropdown" data-dropdown-content>
+						  <li><a href="/display/addBatch.php">Add New Batch</a></li>
+						  <li><a href="/display/batches.php">View/Search All Batches</a></li>
+						</ul>
+			  	
 			</div>
 		</div>
 
