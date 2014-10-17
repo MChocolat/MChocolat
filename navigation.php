039fac78922fe79bba48de12475f9bc165521149
Geo <geoving.gerard@gmail.com>
2014-10-17 15:21:59 -0400

    newNavbar test

--- navigation.php
+++ navigation.php
@@ -1,52 +1,60 @@
 <!-- Navigation -->
 
-<div class="row">
-  	<div class="small-8 large-centered columns">
-		<p></p>
-		<div class="button-bar">
-				<li>
-					<a href="#" class="button" data-dropdown="ingredients" data-dropdown="drop">Ingredients</a>
-					<ul id="ingredients" class="f-dropdown" data-dropdown-content>
-					  <li><a href="/display/addIngredient.php">Add New Ingredient</a></li>
-					  <li><a href="/display/ingredients.php">View/Search All Ingredients</a></li>
-					</ul>
-				</li>
+<body>
+	<div class="row">
+	  	<div class="small-8 large-centered columns">
+			<p></p>
+			<div class="button-bar">
+				<ul class="button-group">
+					<li>
+						<a href="#" class="button" data-dropdown="ingredients" data-options="is_hover:true">Ingredients</a>
+						<ul id="ingredients" class="f-dropdown" data-dropdown-content>
+						  <li><a href="/display/addIngredient.php">Add New Ingredient</a></li>
+						  <li><a href="/display/ingredients.php">View/Search All Ingredients</a></li>
+						</ul>
+					</li>
 
-		    	<li>
-					<a href="#" class="button" data-dropdown="batches" data-options="is_hover:true">Batches</a>
-					<ul id="batches" class="f-dropdown" data-dropdown-content>
-					  <li><a href="/display/addBatch.php">Add New Batch</a></li>
-					  <li><a href="/display/batches.php">View/Search All Batches</a></li>
-					</ul>
-		    	</li>
+			    	<li>
+						<a href="#" class="button" data-dropdown="batches" data-options="is_hover:true">Batches</a>
+						<ul id="batches" class="f-dropdown" data-dropdown-content>
+						  <li><a href="/display/addBatch.php">Add New Batch</a></li>
+						  <li><a href="/display/batches.php">View/Search All Batches</a></li>
+						</ul>
+			    	</li>
 
-		    	<li>
-		    		<a href="#" class="button" data-dropdown="recipes" data-options="is_hover:true">Recipes</a>
-					<ul id="recipes" class="f-dropdown" data-dropdown-content>
-					  <li><a href="/display/addRecipe.php">Add New Recipe</a></li>
-					  <li><a href="/display/recipes.php">View/Search All Recipes</a></li>
-					</ul>
-		    	</li>
+			    	<li>
+			    		<a href="#" class="button" data-dropdown="recipes" data-options="is_hover:true">Recipes</a>
+						<ul id="recipes" class="f-dropdown" data-dropdown-content>
+						  <li><a href="/display/addRecipe.php">Add New Recipe</a></li>
+						  <li><a href="/display/recipes.php">View/Search All Recipes</a></li>
+						</ul>
+			    	</li>
 
-		    	<li>
-		    		<a href="#" class="button" data-dropdown="boxLabels" data-options="is_hover:true">Box Labels</a>
-					<ul id="boxLabels" class="f-dropdown" data-dropdown-content>
-					  <li><a href="/display/createBoxLabel.php">Add New Box Label</a></li>
-					  <li><a href="/display/boxLabels.php">View/Search All Box Labels</a></li>
-					</ul>
-		    	</li>
-			<!--
-		    	<li>
-		    		<a href="#" class="button" data-dropdown="packaging" data-options="is_hover:true">Packaging</a>
-					<ul id="packaging" class="f-dropdown" data-dropdown-content>
-					  <li><a href="#">Packaging Option #1</a></li>
-					  <li><a href="#">Packaging Option #2</a></li>
-					</ul>
-		    	</li>
-		    -->
-		</div>
-  	</div>
-</div>
+			    	<li>
+			    		<a href="#" class="button" data-dropdown="boxLabels" data-options="is_hover:true">Box Labels</a>
+						<ul id="boxLabels" class="f-dropdown" data-dropdown-content>
+						  <li><a href="/display/createBoxLabel.php">Add New Box Label</a></li>
+						  <li><a href="/display/boxLabels.php">View/Search All Box Labels</a></li>
+						</ul>
+			    	</li>
+				<!--
+			    	<li>
+			    		<a href="#" class="button" data-dropdown="packaging" data-options="is_hover:true">Packaging</a>
+						<ul id="packaging" class="f-dropdown" data-dropdown-content>
+						  <li><a href="#">Packaging Option #1</a></li>
+						  <li><a href="#">Packaging Option #2</a></li>
+						</ul>
+			    	</li>
+			    -->
+			  	</ul>
+			</div>
+	  	</div>
+	</div>
+
+	<script src="js/vendor/jquery.js"></script>
+	<script src="js/foundation/foundation.js"></script>
+	<script src="js/foundation/foundation.dropdown.js"></script>
+</body>
 
 
 
