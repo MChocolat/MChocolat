7e8c268f216ed96031a9aac2553367e2745fb269
Geo <geoving.gerard@gmail.com>
2014-10-17 21:44:54 -0400

    updateRecipe tweak

--- display/batches.php
+++ display/batches.php
@@ -12,67 +12,68 @@
   	<body>
 		<div class="row ">
 			<div id="editBatchSection" class="large-5 columns">
-	  			
-	  			<div class="h3">Update Batch</div>
-	  			<p></p>
-		  			
-	  			<div class="row">
-					<div class="large-12 medium-4 columns">
-				  		<input id="editIdInput" type = "text" placeholder = "Name">
-				  	</div>
-				</div>
+		  		<div class="panel">
+		  			<div class="h3">Update Batch</div>
+		  			<p></p>
+			  			
+		  			<div class="row">
+						<div class="large-12 medium-4 columns">
+					  		<input id="editIdInput" type = "text" placeholder = "Name">
+					  	</div>
+					</div>
 
-				<div class="row">
-					<div class="large-12 medium-4 columns">
-				  		<input id="editUpcInput" type = "text" placeholder = "Product ID Number">
-				  	</div>
-				</div>
+					<div class="row">
+						<div class="large-12 medium-4 columns">
+					  		<input id="editUpcInput" type = "text" placeholder = "Product ID Number">
+					  	</div>
+					</div>
 
-				<div class="row">
-					<div class="large-12 medium-4 columns">
-				  		<input id="editDopInput" type = "text" placeholder = "Quantity">
-				  	</div>
-				</div>
+					<div class="row">
+						<div class="large-12 medium-4 columns">
+					  		<input id="editDopInput" type = "text" placeholder = "Quantity">
+					  	</div>
+					</div>
 
-				<div class="row">
-				  	<div class="large-12 medium-4 columns">
-				  		<input id="editExpInput" type = "text" placeholder = "Purchase Date">
-				  	</div>
-				</div>
-				
-				<div class="row">
-				  	<div class="large-12 medium-4 columns">
-				  		<input id="editPriceInput" type = "text" placeholder = "Expiration Date">
-				  	</div>
-				</div>
+					<div class="row">
+					  	<div class="large-12 medium-4 columns">
+					  		<input id="editExpInput" type = "text" placeholder = "Purchase Date">
+					  	</div>
+					</div>
+					
+					<div class="row">
+					  	<div class="large-12 medium-4 columns">
+					  		<input id="editPriceInput" type = "text" placeholder = "Expiration Date">
+					  	</div>
+					</div>
 
-				<div class="row">
-				  	<div class="large-12 medium-4 columns">
-				  		<input id="editDistInput" type = "text" placeholder = "Manufacturer">
-				  	</div>
-				</div>
+					<div class="row">
+					  	<div class="large-12 medium-4 columns">
+					  		<input id="editDistInput" type = "text" placeholder = "Manufacturer">
+					  	</div>
+					</div>
 
-				<div class="row">
-				  	<div class="large-12 medium-4 columns">
-				  		<input id="editSubInput" type = "text" placeholder = "Price">
-				  	</div>
-				</div>
-					  
-				<p></p>
+					<div class="row">
+					  	<div class="large-12 medium-4 columns">
+					  		<input id="editSubInput" type = "text" placeholder = "Price">
+					  	</div>
+					</div>
+						  
+					<p></p>
 
-				<div class="">
-				  	<div class="small-11 small-centered columns">
-					    <ul class="button-group even-3">
-					  		<li><a href="#clear" class="tiny button">Clear Entries</a></li>
-					  		<li><a href="#barcode" class="tiny button">Scan Barcode</a></li>
-					  		<li><a id="addIngredientButton" class="tiny button">Update Batch</a></li>
-						</ul>
+					<div class="">
+					  	<div class="small-11 small-centered columns">
+						    <ul class="button-group even-3">
+						  		<li><a href="#clear" class="tiny button">Clear Entries</a></li>
+						  		<li><a href="#barcode" class="tiny button">Scan Barcode</a></li>
+						  		<li><a id="addIngredientButton" class="tiny button">Update Batch</a></li>
+							</ul>
+						</div>
 					</div>
 				</div>
 		  	</div>
 	
 			<!-- Search Bar / Inventory Table --> 
-  			<div class="large-7 columns">
+  			<div class="large-7 medium-4 columns">
 				<table id="batchesTable" class="display">
 				 	<thead>
 					    <tr>
