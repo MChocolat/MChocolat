d56dabdebcf9617cfe13ec940d152f00a51e2fe7
rvz7 <rvbroussard@gmail.com>
2014-10-23 15:54:19 -0400

    Revert 26c1a80..f1e33a3
    This rolls back to commit 26c1a80264695d2fe6aea25bde93b74ab75a0dfe.

--- js/addIngredients.js
+++ js/addIngredients.js
@@ -3,6 +3,34 @@ $(document).ready( function () {
 		if(e.which == 13) {
 			if (document.activeElement.id == 'addUpcInput') {
 				alert ('You Pressed Enter in UPC!');
+					// Load all Ingredients  
+				$.ajax({
+					type: "POST",
+					url: '/functions.php',
+					cache: false,
+					data: {'action': 'ingrUPCLookup'
+							'$data': document.activeElement.id.getVal()},
+					success: function(data, status) {
+						ingredientsList = data;
+						self.ingredientsTable = $('#ingredientsTable').dataTable({
+							"aaData": jQuery.parseJSON(ingredientsList),
+							"aoColumns": [
+								{"sTitle": "ID", "mData": 'IngrID' },
+								{"mData": 'UPC' },
+								{"mData": 'DOP' },
+								{"mData": 'Exp' },
+								{"mData": 'Price' },
+								{"mData": 'Distributor' },
+								{"mData": 'SubIngr' }
+							]
+						});
+				
+				} );
+					},
+					error: function(xhr, desc, err) {
+						alert("SOME ERROR LOADING RAW MATERIALS");
+					}
+				});
 			}
 		}
 	});
