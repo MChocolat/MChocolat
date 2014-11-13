421958e505424a6e8c84f8704c8ebab679f19cef
Aaryn Glosterm <agg1991@yahoo.com>
2014-11-13 12:59:10 -0500

    testing

--- display/recipes.php
+++ display/recipes.php
@@ -6,7 +6,7 @@
 		<link rel="stylesheet" type="text/css" href="/css/dataTables.foundation.css"/>
 		<script src="/js/jquery.dataTables.js"></script>
 		<script type="text/javascript" language="javascript" src="/js/dataTables.foundation.js"></script>
-		<script type="text/javascript" language="javascript" src="/js/jquery.simplemodal.1.4.4.min.js"></script>
+		<script type="text/javascript" language="javascript" src="/js/jquery-ui-1.11.2/jquery-ui.min.js"></script>
 		<script type="text/javascript" src="/js/recipes.js"></script>
   	</head>
   
