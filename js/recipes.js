ed37c000de0ec5f7e13c536a0ae54afa6ce76947
Aaryn Glosterm <agg1991@yahoo.com>
2014-11-17 03:43:49 -0500

    update row text

--- js/recipes.js
+++ js/recipes.js
@@ -263,7 +263,7 @@ function updateRecipeIngredients(recipeID){
 			cache: false,
 			data: {'action': 'updateRecipeIngredients', 'data': data},
             success: function (data, status) {
-				self.selectedRow[0].innerText($('#editNameInput').val());
+				$(self.selectedRow[0]).text($('#editNameInput').val());
 				$('ingredientsDiv').empty();
 				saveSuccessful();
 			}
