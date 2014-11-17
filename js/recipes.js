b19e5a40a214311b2179323a839b3ba30cd6026b
Aaryn Glosterm <agg1991@yahoo.com>
2014-11-17 00:49:42 -0500

    testing modal open

--- js/recipes.js
+++ js/recipes.js
@@ -55,6 +55,9 @@ $(document).ready( function () {
 	$("#editRecipeButton").bind("click", editRecipe);
 	$("#deleteRecipeButton").bind("click", deleteRecipe);
 	
+	$("#dialog").dialog({ autoOpen: false,
+							width: 800,
+							height: 800});
 	
 } );
 
@@ -95,7 +98,7 @@ function removeRecipeRow(){
 }
 
 function editRecipe(){
-	$('#dialog').foundation('reveal', 'open');
+	$( "#dialog" ).dialog( "open" );
 	
 	// Create a way to add multiple ingredients to the existing ingredients
 	for(i = 0; i < 15; i++){
