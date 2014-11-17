c0c315c339f9deb82c45f85a968189a20a23c5de
Christina <christina.snyder11@gmail.com>
2014-11-16 23:27:10 -0500

    starting box label implementation
    making appropriate pages and using add batch as a starting point

--- display/createBoxLabel.php
+++ display/createBoxLabel.php
@@ -5,80 +5,35 @@
 		<link rel="stylesheet" type="text/css" href="/css/dataTables.foundation.css"/>
 		<script src="/js/jquery.dataTables.js"></script>
 		<script type="text/javascript" language="javascript" src="/js/dataTables.foundation.js"></script>
-		<script type="text/javascript" src="/js/boxLabels.js"></script>
-  	</head>
+		<script type="text/javascript" src="/js/createBoxLabel.js"></script>
+	</head>
   
   	<body>
-		<!-- Add BoxLabel -->
-		<div class="row">
-			<div id="addBoxLabelSection" class="large-5 large-centered columns">
-		  		<div class="panel">
-		  			<div class="h3">New Box Label</div>
-		  			<p></p>
-
-		  			<div class="row">
-						<div class="large-12 medium-4 columns">
-					  		<input id="addIdInput" type = "text" placeholder = "ID">
-					  	</div>
-					</div>
-
-					<div class="row">
-						<div class="large-12 medium-4 columns">
-					  		<input id="addUpcInput" type = "text" placeholder = "UPC">
-					  	</div>
-					</div>
-
-					<div class="row">
-						<div class="large-12 medium-4 columns">
-					  		<input id="addDopInput" type = "text" placeholder = "Date of Purchase">
-					  	</div>
-					</div>
-
-					<div class="row">
-					  	<div class="large-12 medium-4 columns">
-					  		<input id="addExpInput" type = "text" placeholder = "Purchase Date">
-					  	</div>
-					</div>
-					
-					<div class="row">
-					  	<div class="large-12 medium-4 columns">
-					  		<input id="addPriceInput" type = "text" placeholder = "Price">
-					  	</div>
-					</div>
-
-					<div class="row">
-					  	<div class="large-12 medium-4 columns">
-					  		<input id="addDistInput" type = "text" placeholder = "Distributor">
-					  	</div>
-					</div>
-
-					<div class="row">
-					  	<div class="large-12 medium-4 columns">
-					  		<input id="addSubInput" type = "text" placeholder = "Sub BoxLabels">
-					  	</div>
-					</div>
-					  
+		<div class="row ">
+			<div id="addBatchSection" class="large-6 columns">
+				<div class="panel">
+		  			<div class="h3">How many chocolates are in your box?</div>
 					<p></p>
-
-					<div class="">
-					  	<div class="small-11 small-centered columns">
-						    <ul class="button-group even-3">
-						  		<li><a href="#clear" class="tiny button">Clear Entries</a></li>
-							  		<li><a href="#" data-reveal-id="barcode" class="tiny button">Scan Barcode</a></li>
-						  		<li><a id="addIngredientButton" class="tiny button">Add Label</a></li>
-								</ul>
-
-								<div id="barcode" class="reveal-modal" data-reveal>
-								  <h2>Barocde Scanner.</h2>
-								  <p>Please scan the barcode!</p>
-								  <a class="close-reveal-modal">&#215;</a>
-								</div>
-
-							</div>
+						
+					<div class="row">
+						<div class="small-9 columns">
+							<select id="recipeSelect"></select>
+						</div>
+						<div class="small-3 columns">
+							<a id="selectRecipeButton" class="tiny button">Select</a>
+						</div>
 					</div>
-		  		</div>
+						
+					<p></p>
+				</div>
 			</div>
-		</div>
+
+			
+			<div id="ingredientsDiv" class="large-6 columns panel">
+			
+			
+			</div>
+		</div>		
 
 	<script src="js/vendor/jquery.js"></script>
     <script src="js/foundation.min.js"></script>
---
+++ js/createBoxLabel.js
@@ -0,0 +1,155 @@
+var self;
+var recipesList;
+var ingredientsList;
+var recipeID;
+
+// Function for when page first loads, what you want it to do
+$(document).ready( function () {
+    self = this;
+	
+	// Load all Recipes  
+	$.ajax({
+		type: "POST",
+		url: '/functions.php',
+		cache: false,
+		data: {'action': 'getRecipes'},
+		success: function(data, status) {
+			recipesList = jQuery.parseJSON(data);
+			
+			for (var i = 0; i<recipesList.length; i++) {
+				document.getElementById('recipeSelect').options[i] = new Option(recipesList[i]['RecipeName']);
+			}
+		},
+		error: function(xhr, desc, err) {
+		}
+	});
+	
+	//Set Button Functions
+	$("#selectRecipeButton").bind("click", selectRecipe);
+	$("#barcodeButton").bind("click", addIngredients);
+	$("#addBatchButton").bind("click", addBatch);
+	
+} );
+
+function clearBatchIngredients(){
+	$('#ingredientsDiv').empty();
+	ingredientsList = [];
+}
+
+function createIngredientRow(i){
+	var div = document.createElement("div");
+	$(div).addClass("row");
+	self.getElementById('ingredientsDiv').appendChild(div);
+	
+	
+	var nameDiv = document.createElement("div");
+	var upcDiv = document.createElement("div");
+	
+	$(nameDiv).addClass("large-6 columns");
+	$(upcDiv).addClass("large-6 columns");
+
+	var name = document.createElement("p");
+	var t = document.createTextNode(ingredientsList[i]['IngrName']);
+	name.appendChild(t);
+	
+	var upc = document.createElement("input");
+	upc.type = "text";
+	upc.placeholder = "Ingredient UPC";
+	
+	nameDiv.appendChild(name);
+	upcDiv.appendChild(upc);
+	
+	div.appendChild(nameDiv);
+	div.appendChild(upcDiv);	
+}
+
+function selectRecipe(){
+	clearBatchIngredients();
+
+	recipeID = getRecipeID($("#recipeSelect option:selected").text());
+
+	//Load ingredients for selected recipe
+	$.ajax({
+		type: "POST",
+		url: '/functions.php',
+		cache: false,
+		data: {'action': 'getRecipeIngredients', 'data': recipeID},
+		success: function(data, status) {
+			ingredientsList = jQuery.parseJSON(data);
+			
+			for (var i = 0; i<ingredientsList.length; i++) {
+				createIngredientRow(i);
+			}
+		},
+		error: function(xhr, desc, err) {
+		}
+	});
+	
+	
+}
+
+function getRecipeID(recipe){
+	var ID;
+	for (var i = 0; i<recipesList.length; i++) {
+		if (recipesList[i]['RecipeName'] == recipe){
+			ID = recipesList[i]['RecipeID'];
+		}
+	}
+	return ID.toString();
+}
+
+function addBatch(){
+	//Save batch to DB
+	
+	var data = {"RecipeID": recipeID};
+	$.ajax({
+            type: 'POST',
+            url: '/functions.php',
+			cache: false,
+			data: {'action': 'addBatch', 'data': data},
+            success: function (data, status) {
+				//Do another AJAX save ingredients associated with recipe to ingrRecipe table
+				addBatchIngredients(data);
+			}
+    });
+}
+
+function addBatchIngredients(batchID){
+	//TODO: form validation
+	var data = new Array();
+
+	var ingredients = $(ingredientsDiv).children();
+	
+	var upc;
+	
+	for(i = 0; i < ingredients.size(); i++){
+		upc = $($($(ingredients[i]).children()[1]).children()[0]).val()
+		var row = {"BID": batchID, "IngrID":upc};
+		data[i] = row;
+	}
+
+	$.ajax({
+            type: 'POST',
+            url: '/functions.php',
+			cache: false,
+			data: {'action': 'addBatchIngredients', 'data': data},
+            success: function (data, status) {
+				saveSuccessful();
+			}
+    });
+
+}
+
+function saveSuccessful(){
+	alert("SAVED");
+}
+
+
+
+
+function addIngredients(){
+	// TODO: actually take barcode info and create rows
+	for(i = 0; i < 10; i++){
+		createIngredientRow();
+	}	
+}
