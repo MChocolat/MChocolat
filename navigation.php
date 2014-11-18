350293f03cc4438ecd1936e23fe933738815ca80
Christina <christina.snyder11@gmail.com>
2014-11-18 17:16:54 -0500

    Revert "more slashes"
    This reverts commit d61b2a03ad495009b739ff18f3a2b135a3790bad.

--- navigation.php
+++ navigation.php
@@ -55,8 +55,8 @@
 			</div>
 		</div>
 
-		<script src="/js/vendor/jquery.js"></script>
-	    <script src="/js/foundation.min.js"></script>
+		<script src="js/vendor/jquery.js"></script>
+	    <script src="js/foundation.min.js"></script>
 	    <script> $(document).foundation(); </script>
  	</body>
 </html>
