aa9b9069f6cf3f11f1597bd914748404b323060b
Aaryn Glosterm <agg1991@yahoo.com>
2014-11-20 01:06:46 -0500

    testing data barcode parsing

--- display/print.php
+++ display/print.php
@@ -8,10 +8,27 @@
 	
 	<script>
    $(document).ready(function(){
+		
 				generateI25 = function(){
 					$('.barcodeI25').barcode({code:'I25'});
 				};}
+				
+				var IRID = getUrlParameter('param');
+				
 	);
+	
+	function getUrlParameter(sParam){
+		var sPageURL = window.location.search.substring(1);
+		var sURLVariables = sPageURL.split('&');
+		for (var i = 0; i < sURLVariables.length; i++) 
+		{
+			var sParameterName = sURLVariables[i].split('=');
+			if (sParameterName[0] == sParam) 
+			{
+				return sParameterName[1];
+			}
+		}
+	} 
 	window.print();
     </script>
 	<meta http-equiv="refresh" content="0; url=/display/addIngredient.php" />
