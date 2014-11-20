73ca990206670e87e6aba7c8d8f25298734f5e4b
Aaryn Glosterm <agg1991@yahoo.com>
2014-11-20 01:10:20 -0500

    test

--- display/print.php
+++ display/print.php
@@ -3,7 +3,6 @@
 	<script src="/js/jquery.barcode.0.3.js"></script>
 	<meta charset="utf-8" />
 	<div data-param="foo" class="barcodeI25" style="width:153px;height:25px;border:1px solid red;">
-		<?php SELECT IngrID FROM ingredients WHERE UPC = $UPC ORDER BY IngrID DESC LIMIT 1; ?>
 	</div>
 	
 	<script>
@@ -14,6 +13,7 @@
 				};}
 				
 				var IRID = getUrlParameter('param');
+			
 				
 	);
 	
