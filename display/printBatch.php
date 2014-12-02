ded6c35fdf51282bad9ed831d4550afb5b55c83a
cwalker43 <fatboy95ci@yahoo.com>
2014-12-02 16:55:42 -0500

    iHow do i code

--- display/printBatch.php
+++ display/printBatch.php
@@ -21,7 +21,7 @@
 				$('.barcodeI25').text(IRID);
 				generateI25();
 				var recipeName=getUrlParameter('param2');
-				decodeURIComponent(recipeName);
+				recipeName=decodeURIComponent(recipeName);
 				$('.recipeName').text(recipeName);
 				window.print();
 	});
